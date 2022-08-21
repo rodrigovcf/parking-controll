f8505757a758fce48faa952744fe4b937ce73756
rodrigovcf <rodrigo.rvcf@gmail.com>
2022-08-20 23:24:44 -0300

    Add projeto base

---
+++ parking-controll/src/main/java/com/parkingcontroll/parkingcontroll/ParkingControllApplication.java
@@ -0,0 +1,13 @@
+package com.parkingcontroll.parkingcontroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingControllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControllApplication.class, args);
+	}
+
+}
---
+++ parking-controll/src/test/java/com/parkingcontroll/parkingcontroll/ParkingControllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkingcontroll.parkingcontroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
